f35487ab9b3ca9a3fc8826310be183cf129d7d7b
GitProgramm <59909720+GitProgramm@users.noreply.github.com>
2020-03-01 19:01:39 +0200

    Update JS.js

--- js/JS.js
+++ js/JS.js
@@ -1,15 +1,14 @@
 
 window.onscroll = function() {myFunction()};
-console.log("a");
 var navbar = document.getElementById("myNavbar");
-
+var nav1 = documnet.getElementById("myNav");
 var sticky = navbar.offsetTop;
-
 function myFunction() {
-  console.log("b");
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
+    nav1.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
+    nav1.classList.remove("sticky");
   }
 }
