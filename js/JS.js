8db50de9a574324c581785272a7bfc6d65a274af
GitProgramm <59909720+GitProgramm@users.noreply.github.com>
2020-03-01 18:44:53 +0200

    Update JS.js

--- js/JS.js
+++ js/JS.js
@@ -1,11 +1,12 @@
 
 window.onscroll = function() {myFunction()};
-
+console.log("a");
 var navbar = document.getElementById("myNavbar");
 
 var sticky = navbar.offsetTop;
 
 function myFunction() {
+  console.log("b");
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
